ee2ebbb7915e78ce79595da907315374b4202766
MrLeoufff <mrleoufff@gmail.com>
2023-12-30 19:20:22 +0100

    first commit

---
+++ JustePrix/justeprix.js
@@ -0,0 +1,106 @@
+import { Conffeti } from "../lib/conffeti.js";
+// Générer un chiffre en aléatoire
+// L'utilisateur fera ,des propositions
+// Continuer tant que le prix n'est pas bon
+
+let NumberToFind = 0;
+const resultDiv = document.getElementById("resultDiv");
+const reboursDiv = document.getElementById("compteARebours");
+const GamePropalDiv = document.getElementById("GamePropalDiv");
+let TempsRestant = 0;
+let compteurInterval = null;
+
+document.getElementById("beginGame").addEventListener("click", function () {
+  // Lancer la partie
+  // Récuperer un chiffre aléatoire
+  launchGame();
+});
+
+document
+  .getElementById("checkPropalButton")
+  .addEventListener("click", function () {
+    checkPropal();
+  });
+
+document
+  .getElementById("userPropalInput")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      checkPropal();
+    }
+  });
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function checkPropal() {
+  let numberPropal = document.getElementById("userPropalInput").value;
+  if (NumberToFind > numberPropal) {
+    // C'est plus
+    resultDiv.innerHTML = "C'est plus !";
+    let audio = new Audio("audio/plus.mp3");
+    audio.play();
+  } else if (NumberToFind < numberPropal) {
+    // C'est moins
+    resultDiv.innerHTML = "C'est moins !";
+    let audio = new Audio("audio/moins.mp3");
+    audio.play();
+  } else if (NumberToFind == numberPropal) {
+    // Gagné
+    resultDiv.innerHTML = "C'est gagné !";
+
+    endGame(true);
+  }
+}
+
+function launchGame() {
+  Conffeti.stopAnimationConffeti();
+  NumberToFind = getRandomInt(1000);
+  // Compte a rebours
+  console.log(NumberToFind);
+  TempsRestant = 30;
+  GamePropalDiv.style.display = "block";
+  if (compteurInterval != null) {
+    clearInterval(compteurInterval);
+  }
+  compteurInterval = setInterval(() => {
+    reboursDiv.innerHTML = TempsRestant;
+    TempsRestant--;
+
+    if (TempsRestant >= 20) {
+      reboursDiv.classList.add("cool");
+      reboursDiv.classList.remove("warning");
+      reboursDiv.classList.remove("danger");
+    } else if (TempsRestant > 10) {
+      reboursDiv.classList.add("warning");
+      reboursDiv.classList.remove("cool");
+      reboursDiv.classList.remove("danger");
+    } else if (TempsRestant >= 0) {
+      reboursDiv.classList.add("danger");
+      reboursDiv.classList.remove("cool");
+      reboursDiv.classList.remove("warning");
+    } else if (TempsRestant < 0) {
+      clearInterval(compteurInterval);
+      // Partie terminer
+
+      endGame(false);
+    }
+  }, 1000);
+}
+
+function endGame(gagne) {
+  if (gagne) {
+    Conffeti.launchAnimationConffeti();
+    let audio = new Audio("audio/gagné.mp3");
+    audio.play();
+    setTimeout(() => {
+      Conffeti.stopAnimationConffeti();
+    }, 5000);
+  } else {
+    let audio = new Audio("audio/tes_mauvais_jack.mp3");
+    audio.play();
+  }
+  GamePropalDiv.style.display = "none";
+  clearInterval(compteurInterval);
+}
